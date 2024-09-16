545cee276011369b49f6f3313908a90dcde75397
Stephen Hodgson <rage.against.the.pixel@gmail.com>
2024-09-16 14:17:38 -0400

    unity-xcode-builder@v1.0.0 (#1)
    - Initial Release! 🎉🚀

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,39 @@
-import core from '@actions/core';
+import core = require('@actions/core');
+import exec = require('@actions/exec');
+import {
+    ImportCredentials,
+    Cleanup
+} from './credentials';
+import {
+    GetProjectDetails,
+    ArchiveXcodeProject,
+    ExportXcodeArchive
+} from './xcode';
 
 const IS_POST = !!core.getState('isPost');
 
 const main = async () => {
     try {
         if (!IS_POST) {
-            core.info('Hello World!');
             core.saveState('isPost', true);
+            const xcodeVersion = core.getInput('xcode-version');
+            if (xcodeVersion) {
+                core.info(`Setting xcode version to ${xcodeVersion}`);
+                await exec.exec('sudo', ['xcode-select', '-s', `/Applications/Xcode_${xcodeVersion}.app/Contents/Developer`]);
+            }
+            await exec.exec('xcodebuild', ['-version']);
+            const credential = await ImportCredentials();
+            let projectRef = await GetProjectDetails();
+            projectRef.credential = credential;
+            projectRef = await ArchiveXcodeProject(projectRef);
+            core.setOutput('executable', projectRef.archivePath);
+            projectRef = await ExportXcodeArchive(projectRef);
+            core.setOutput('output-directory', projectRef.exportPath);
         } else {
-            core.info('Hello World! (post)');
+            await Cleanup();
         }
     } catch (error) {
-        core.setFailed(error);
+        core.setFailed(error.stack);
     }
 }
 
